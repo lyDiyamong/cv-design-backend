a3e2edcde2ef3aca548c2091caf73fca52739eff
lyDiyamong <diyamong26@gmail.com>
2025-01-07 11:47:17 +0700

    add: connect app to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { configDotenv } from 'dotenv';
 
+configDotenv();
 @Module({
-  imports: [AuthModule, CoreModule],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URI, {
+      connectionFactory: (connection) => {
+        const logger = new Logger('Mongoose');
+
+        connection.on('connected', () => {
+          logger.log('MongoDB connected successfully.');
+        });
+
+        connection.on('error', (error) => {
+          logger.error(`MongoDB connection error: ${error.message}`);
+        });
+        return connection;
+      },
+    }),
+    AuthModule,
+    CoreModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configDotenv } from 'dotenv';
 
 async function bootstrap() {
+  console.log('DB', process.env.DB_URI);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log('App is listening to', process.env.PORT);
+  });
 }
 bootstrap();
