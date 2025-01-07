a9794b4db70ce8f0d9a3f1a3c3348e7882e0f6b9
lyDiyamong <diyamong26@gmail.com>
2025-01-07 18:58:10 +0700

    fix : delete console

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,10 +1,12 @@
 import * as bcrypt from 'bcryptjs';
 
-export async function hashPassword(value: string) {
-  console.log('password', value);
+export async function hashPassword(value: string): Promise<string> {
   return bcrypt.hash(value, 10);
 }
 
-export async function comparePassword(compareValue: string, hashValue: string) {
+export async function comparePassword(
+  compareValue: string,
+  hashValue: string,
+): Promise<boolean> {
   return bcrypt.compare(compareValue, hashValue).catch(() => false);
 }
